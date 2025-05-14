b5023dd85e5a5f7c55367e10542e3048fe232ad8
QWQ123321123 <2479293858@qq.com>
2025-05-14 21:49:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def index():
     return render_template_string(HTML, result=result)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=8000)
